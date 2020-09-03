183ee1c2f3874808e35075c57776a9984cf9d2eb
skuzzymiglet <skuzzymiglet@gmail.com>
2020-09-03 12:39:56 +0100

    Concurrent fetching (yay!)

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module git.sr.ht/~skuzzymiglet/feef
 
 go 1.15
+
+require (
+	github.com/kr/pretty v0.2.1
+	github.com/mmcdole/gofeed v1.0.0
+	github.com/pkg/profile v1.5.0
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/mmcdole/gofeed"
+	"github.com/pkg/profile"
+	log "github.com/sirupsen/logrus"
+)
+
+// func getFeed(url string, wg *sync.WaitGroup) (*gofeed.Feed, error) {
+func getFeed(url string) (*gofeed.Feed, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	parser := gofeed.NewParser()
+	b, err := ioutil.ReadAll(resp.Body)
+	s := string(b)
+	return parser.ParseString(s)
+}
 
 func main() {
-	fmt.Println("hih")
+	defer profile.Start().Stop()
+	start := time.Now()
+	urlsFile, err := os.Open("urls")
+	if err != nil {
+		log.Fatal(err)
+	}
+	urlNames, err := ioutil.ReadAll(urlsFile)
+	urls := strings.Split(string(urlNames), "\n")
+	urls = urls[0 : len(urls)-1]
+	var wg sync.WaitGroup
+	for i, v := range urls {
+		wg.Add(1)
+		go func(i int, v string, wg *sync.WaitGroup) {
+			start := time.Now()
+			_, err := getFeed(v)
+			end := time.Now()
+			if err != nil {
+				log.Warnln(i, v, err)
+			} else {
+				log.Infof("Fetched feed %d (%s) in %s\n", i, v, end.Sub(start))
+			}
+			wg.Done()
+		}(i, v, &wg)
+	}
+	wg.Wait()
+	end := time.Now()
+	log.Infoln("Fetched all in", end.Sub(start))
 }
