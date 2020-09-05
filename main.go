55ef90a3c30d2defb4c398c84d46354e8be9033d
skuzzymiglet <skuzzymiglet@gmail.com>
2020-09-05 09:51:36 +0100

    Fetch() is back

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"strings"
 	"sync"
 
 	"github.com/dgraph-io/badger"
@@ -13,54 +9,70 @@ import (
 	"github.com/pkg/profile"
 
 	"github.com/bartmeuris/progressio"
+	log "github.com/sirupsen/logrus"
 )
 
+// Feeds holds RSS/Atom feeds and a database. It provides download/parse progress and a channel for feed updates
 type Feeds struct {
 	Feeds     []gofeed.Feed
 	db        *badger.DB
 	updates   chan *gofeed.Feed
-	httpMutex *sync.Mutex
+	httpMutex *sync.Mutex // Currently unused
 }
 
 type _ interface {
-	SetURLs([]string) error
 	NotifyUpdates() <-chan *gofeed.Feed
-	Fetch(string) (<-chan progressio.Progress, error)
-	FetchAll() (<-chan struct {
-		url string
-		p   progressio.Progress
-	}, error)
+	Fetch(progressChans map[string]chan progressio.Progress) error
 }
 
-func getFeed(url string, m *sync.Mutex) (*gofeed.Feed, error) {
-	m.Lock()
-	resp, err := http.Get(url)
-	m.Unlock()
-	if err != nil {
-		return nil, err
-	}
+type multiProgress struct {
+	url string
+	v   progressio.Progress
+}
+
+// Fetch fetches the keys of progressChans and sends progress to their value
+func (f *Feeds) Fetch(urls []string, progress chan multiProgress) error {
 	parser := gofeed.NewParser()
-	b, err := ioutil.ReadAll(resp.Body)
-	s := string(b)
-	return parser.ParseString(s)
-}
-
-func (f *Feeds) SetURLS([]string) error {
+	for _, v := range urls {
+		var e error
+		go func(e *error, url string, p chan multiProgress) {
+			resp, err := http.Get(url)
+			if err != nil {
+				e = &err
+				return
+			}
+			progressReader, tmp := progressio.NewProgressReader(resp.Body, resp.ContentLength)
+			defer progressReader.Close()
+			go func() {
+				p <- multiProgress{url: url, v: <-tmp}
+			}()
+			f, err := parser.Parse(progressReader)
+			if err != nil {
+				e = &err
+				return
+			}
+			log.Infoln(f)
+			return
+		}(&e, v, progress)
+		if e != nil {
+			return e
+		}
+	}
 	return nil
 }
 
 func main() {
 	defer profile.Start().Stop()
 	var f Feeds
-	urlsFile, err := os.Open("urls2")
-	defer urlsFile.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	b, err := ioutil.ReadAll(urlsFile)
-	strs := strings.Split(string(b), "\n")
-	err = f.SetURLS(strs[:len(strs)-2])
-	if err != nil {
-		log.Fatal(err)
-	}
+	c := make(chan multiProgress, 0)
+	go func() {
+		for v := range c {
+			log.Info(v)
+		}
+	}()
+	log.Warn(f.Fetch([]string{
+		// "https://www.arp242.net/feed.xml",
+		"https://reddit.com/golang.rss",
+	}, c))
+	select {}
 }
