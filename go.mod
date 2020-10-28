5114f53bbd8fe5ed6006ce51ba82ec8a0a5e4aa5
skuzzymiglet <skuzzymiglet@gmail.com>
2020-10-28 21:57:39 +0000

    do it all over again

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module git.sr.ht/~skuzzymiglet/feef
 go 1.15
 
 require (
+	github.com/SlyMarbo/rss v1.0.1
 	github.com/gdamore/tcell v1.4.0
+	github.com/kr/pretty v0.2.1
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7
 	github.com/mmcdole/gofeed v1.1.0
 	github.com/pkg/profile v1.5.0
 	github.com/sirupsen/logrus v1.6.0
+	github.com/skuzzymiglet/gofeed v1.1.2
 )
--- main.go
+++ main.go
@@ -2,97 +2,187 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
-	"io"
-	"io/ioutil"
+	"fmt"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"sync"
-	"time"
 
-	"git.sr.ht/~skuzzymiglet/feef/feeds"
-	"git.sr.ht/~skuzzymiglet/feef/ui"
-
-	"github.com/sirupsen/logrus"
+	"github.com/mmcdole/gofeed"
 )
 
-// NOTE: Main doesn't do much rn, because UI is not fully developed
-func main() {
-	log := logrus.New()
+// LinkedFeedItem is an item linked to its parent feed
+type LinkedFeedItem struct {
+	Feed *gofeed.Feed
+	*gofeed.Item
+}
 
-	var (
-		urlsFile        string
-		refreshInterval time.Duration
-		logFileName     string
-	)
+// LinkedFeed is a feed whose items link to itself
+type LinkedFeed struct {
+	*gofeed.Feed
+	Items []LinkedFeedItem
+}
 
-	flag.StringVar(&urlsFile, "d", "", "file with URLs (one per line)")
-	flag.StringVar(&logFileName, "l", "", "log file")
-	flag.DurationVar(&refreshInterval, "r", time.Second*60, "time between refreshes")
-	flag.Parse()
-
-	urls := make([]string, 0)
-	if urlsFile == "" {
-		flag.Usage()
-		os.Exit(2)
+// LinkFeed links a feed's items and returns a LinkedFeed containing them
+func LinkFeed(f *gofeed.Feed) LinkedFeed {
+	b := make([]LinkedFeedItem, len(f.Items))
+	for i, item := range f.Items {
+		b[i] = LinkedFeedItem{Feed: f, Item: item}
 	}
-	uf, err := os.Open(urlsFile)
-	if err != nil {
-		log.Fatal(err)
+	return LinkedFeed{
+		Feed:  f,
+		Items: b,
 	}
-	s := bufio.NewScanner(uf)
-	for s.Scan() {
-		urls = append(urls, s.Text())
-	}
-	uf.Close()
+}
 
-	var logFile io.Writer
-	if logFileName != "" {
-		logFile, err = os.Create(logFileName)
-		if err != nil {
-			log.Fatal(err)
+// Format implements fmt.Formatter
+func (f LinkedFeed) Format(s fmt.State, c rune) {
+	switch c {
+	case 'd':
+		fmt.Fprint(s, f.Description)
+	case 't':
+		fmt.Fprint(s, f.Title)
+	case 'u':
+		fmt.Fprint(s, f.Link)
+	case 'i':
+		fmt.Fprint(s, f.Link)
+	}
+}
+
+// Format implements fmt.Formatter
+func (f LinkedFeedItem) Format(s fmt.State, c rune) {
+	switch c {
+	case 'd':
+		fmt.Fprint(s, f.Description)
+	case 't':
+		fmt.Fprint(s, f.Title)
+	case 'u':
+		fmt.Fprint(s, f.Link)
+	case 'i':
+		if f.GUID != "" {
+			fmt.Fprint(s, f.GUID)
+		} else {
+			fmt.Fprint(s, f.Link)
 		}
-		defer logFile.(*os.File).Close()
-	} else {
-		logFile = ioutil.Discard
 	}
-	log.SetOutput(logFile)
+}
 
-	// Fetch!
+func (f LinkedFeedItem) String() string {
+	if f.GUID != "" {
+		return f.Feed.Link + "~" + f.GUID
+	}
+	if f.Link != "" {
+		return f.Feed.Link + "~" + f.Link
+	}
+	return "!(NO LINK OR GUID)"
+}
+func (f LinkedFeed) String() string {
+	return f.Feed.Link
+}
 
-	errChan := make(chan error, 0)
+// FindItem finds a feed item
+// q should be a in the format `feed~item`
+// If fuzzy searches if feed or item is not a URL
+func FindItem(q string) (LinkedFeedItem, error) {
+	// TODO: cache
+	var feed, item string
+	parts := strings.SplitN(q, "~", 2)
+	if len(parts) != 2 {
+		return LinkedFeedItem{}, fmt.Errorf("Invalid query: \"%s\"", q)
+	}
+	feed = parts[0]
+	item = parts[1]
+	parser := gofeed.NewParser()
+	// Fetch feed URL (exact)
+	if _, err := url.ParseRequestURI(feed); err == nil {
+		resp, err := http.Get(feed)
+		if err != nil {
+			return LinkedFeedItem{}, fmt.Errorf("FindItem: error downloading %s: %w", feed, err)
+		}
+		defer resp.Body.Close()
+		f, err := parser.Parse(resp.Body)
+		if err != nil {
+			return LinkedFeedItem{}, fmt.Errorf("FindItem: error parsing %s: %w", feed, err)
+		}
+		lf := LinkFeed(f)
+		// Search GUIDs/links (exact)
+		for _, i := range lf.Items {
+			if i.GUID == item || i.Link == item {
+				return i, nil
+			}
+		}
+		// Fuzzy search titles
+		for _, i := range lf.Items {
+			var wordsMatched int
+			words := strings.Split(item, " ")
+			for _, word := range words {
+				if strings.Contains(strings.ToLower(i.Title), strings.ToLower(word)) {
+					wordsMatched++
+				}
+			}
+			if wordsMatched >= len(words) {
+				return i, nil
+			}
+		}
+	} else {
+		// Search urls (this is expensive as hell)
+		// I'm not implementing this until we have a cache
+		// for _, u := range urls {
+		// }
+	}
+	return LinkedFeedItem{}, errors.New("Feed item not found")
+}
 
-	f := feeds.InitFeeds()
+// func FindFeed(q string) (LinkedFeedItem, error) {
+// 	return LinkedFeedItem{}, errors.New("Feed item not found")
+// }
 
-	go func() {
-		wg := sync.WaitGroup{}
-		// NOTE: This is basically a bad do-while
-		// TODO: refactor to reduce code duplication
-		for _, u := range urls {
+func main() {
+	urls := flag.String("u", "urls", "file with newline delimited URLs")
+	query := flag.String("q", "", "query (feed~item)")
+	format := flag.String("f", "%t %u", "format for printing")
+	flag.Parse()
+	switch os.Args[1] {
+	case "list":
+		urlsFile, err := os.Open(*urls)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(urlsFile)
+		var wg sync.WaitGroup
+		for scanner.Scan() {
 			wg.Add(1)
 			go func(u string, wg *sync.WaitGroup) {
+				parser := gofeed.NewParser() // fucking race conditions
 				defer wg.Done()
-				err := f.Fetch(u)
+				resp, err := http.Get(u)
 				if err != nil {
-					errChan <- err
+					fmt.Fprintf(os.Stderr, "error downloading %s: %s\n", u, err)
+					return
 				}
-			}(u, &wg)
+				defer resp.Body.Close()
+				feed, err := parser.Parse(resp.Body)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error parsing %s: %s\n", u, err)
+					return
+				}
+				lf := LinkFeed(feed)
+				fmt.Printf(*format, lf)
+				fmt.Println()
+			}(scanner.Text(), &wg)
 		}
 		wg.Wait()
-		for range time.Tick(refreshInterval) {
-			var wg sync.WaitGroup
-			for _, u := range urls {
-				wg.Add(1)
-				go func(u string, wg *sync.WaitGroup) {
-					defer wg.Done()
-					err := f.Fetch(u)
-					if err != nil {
-						errChan <- err
-					}
-				}(u, &wg)
-			}
-			wg.Wait()
+	case "feeds":
+	case "show":
+		item, err := FindItem(*query)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
-	}()
-	doneChan := make(chan struct{}, 1)
-	ui.RunUI(&ui.FeefUI{DoneChan: doneChan}, doneChan)
+		fmt.Printf(*format, item)
+		fmt.Println()
+	}
 }
